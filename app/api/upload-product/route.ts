b424e98c7ff0602fb73e62ecbe22e85ca728e47f
matiyasjosh <torbaw457@gmail.com>
2026-01-27 21:26:50 +0300

    update: minio working now

--- app/api/upload-product/route.ts
+++ app/api/upload-product/route.ts
@@ -1,26 +1,24 @@
-import { PrismaClient } from "@/lib/generated/prisma";
 import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
-
+import prisma
+ from "@/lib/prisma";
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const {
     name,
-    shortDescription,
+    shortdescription,
     price,
-    brandDesc1,
-    brandDesc2,
-    brandDesc3,
+    branddesc1,
+    branddesc2,
+    branddesc3,
     tag,
     stock,
-    categoryId,
+    categoryid,
     images, // array of MinIO URLs
     } = body;
 
     // Basic validation
-    if (!name || !price || !categoryId || !images?.length) {
+    if (!name || !price || !categoryid || !images?.length) {
     return NextResponse.json(
         { error: "Missing required fields" },
         { status: 400 }
@@ -30,14 +28,14 @@ export async function POST(request: Request) {
     const product = await prisma.product.create({
       data: {
           name,
-          shortDescription,
+          shortdescription,
           price,
-          brandDesc1,
-          brandDesc2,
-          brandDesc3,
+          branddesc1,
+          branddesc2,
+          branddesc3,
           tag,
           stock,
-          categoryId,
+          categoryid,
           images: {
           create: images.map((url: string) => ({ url })),
           },
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
+import { url } from "inspector";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ["127.0.0.1"],
+    remotePatterns: [new URL('http:localhost:9000/**')],
   },
 };
 
